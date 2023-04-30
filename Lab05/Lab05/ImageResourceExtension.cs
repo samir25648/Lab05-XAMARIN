f0670acda204d1ea773fd9c444c92ada9652c690
samir25648 <samir.alanya@tecsup.edu.pe>
2023-04-29 23:00:10 -0500

    three commit

---
+++ Lab05/Lab05/ImageResourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+
+namespace Lab05
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            // Do your translation lookup here, using whatever method you
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
